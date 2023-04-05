6a5273157c739691d0ab61c7c4f235df5576f5f4
sara <sara.atef001@gmail.com>
2023-04-05 11:24:57 +0200

    day03

---
+++ Day03/main.js
@@ -0,0 +1,62 @@
+/** callback function */
+
+// greet(
+//   function () {
+//     console.log("Welcome Mearn track");
+//   },
+
+//   function () {
+//     console.log("Hello ya y");
+//   }
+// );
+// console.log("========");
+// greet(
+//   function () {
+//     console.log("Welcome OS track");
+//   },
+//   function () {
+//     console.log("Test");
+//   }
+// );
+/**
+ * 
+  function greet(x) {
+  x("Ali", 34);
+  console.log("Hello From  ITI");
+}
+
+greet(function (fname, num) {
+  console.log("hello ya " + fname + num);
+});
+
+ */
+
+/**
+ * 
+ * var test = "ali";
+function add() {
+  var test = "mona";
+  var a = 3;
+  var b = 4;
+  console.log(test);
+  return a + b;
+}
+
+var result = add();
+console.log(result);
+console.log(test);
+
+
+function sayHello(trackName) {
+  console.log("Welcome " + trackName);
+}
+
+function greet(name) {
+  sayHello(name);
+  return "Welcome From ITI";
+}
+
+var result = greet("Open source");
+console.log(result);
+
+ */
